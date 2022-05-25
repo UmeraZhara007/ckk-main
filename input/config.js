7567ff6b7b6554dce5f352dfe57f58b915dc8b03
UmeraZhara007 <umeraghori07@gmail.com>
2022-05-26 04:22:08 +0530

    Updated the unique ID fix and overlapping images.

--- input/config.js
+++ input/config.js
@@ -3,27 +3,31 @@ const fs  = require("fs");
 const width = 100;
 const height = 100;
 const dir = __dirname;
-const rarity = [
-    { key: "", val: "original"},
-    { key: "_r", val: "rare"},
-    { key: "_sr", val: "super rare"},
-   ];
-
-   const addRarity = (_str) => {
-     let itemRarity;
-    rarity.forEach((r) => {
-      if (_str.includes(r.key)) {
-        itemRarity = r.val;
-      }
-    });
-    return itemRarity;
-  };
+const description = "This is my cute and creative NFTs.";
+const baseImageUri = "https://crypto_kukdu_ku/nft";
+const startEditionFrom = 1;
+const endEditionAt = 21;
+const editionSize = 21;
+const rarityWeights = [
+  {
+    value: "super_rare",
+    from: 1,
+    to: 2
+  },
+  {
+    value: "rare",
+    from: 3,
+    to: 5
+  },
+  {
+    value: "original",
+    from: 5,
+    to: editionSize
+  },
+]
 
   const cleanName = (_str) => {
     let name = _str.slice(0, -4);
-    rarity.forEach((r) => {
-      name = name.replace(r.key, "");
-    });
     return name;
   };
 
@@ -31,13 +35,11 @@ const rarity = [
      return fs
        .readdirSync(path)
       .filter((item) => !/(^|\/)\.[^\/\.]/g.test(item))
-       .map((i, index) => {
+       .map((i) => {
          return {
-          id: index + 1,
           name: cleanName(i),
-          fileName: i,
-          rarity: addRarity(i),
-        };
+          path: `${path}/${i}`,
+        }
       });
    };
 
@@ -48,39 +50,61 @@ const layers = [
   {
     id: 1,
     name: "All_Background",
-    location: `${dir}/All_Background/`,
-    elements: getElements(`${dir}/All_Background/`),
+    elements: {
+      original: getElements(`${dir}/All_Background/original`),
+      rare: getElements(`${dir}/All_Background/rare`),
+      super_rare: getElements(`${dir}/All_Background/super_rare`),
+    },
     position: {x: 0, y: 0},
     size: { width: width, height: height },
   },
   {    
     id: 2,
     name: "All_Types",
-    location: `${dir}/All_Types/`,
-    elements: getElements(`${dir}/All_Types/`),
+    elements: {
+      original: getElements(`${dir}/All_Types/original`),
+      rare: getElements(`${dir}/All_Types/rare`),
+      super_rare: getElements(`${dir}/All_Types/super_rare`),
+    }, 
     position: {x: 0, y: 0},
     size: { width: width, height: height },
   },
   {
     id: 3,
     name: "Eyes_Accessories",
-    location: `${dir}/Eyes_Accessories/`,
-    elements: getElements(`${dir}/Eyes_Accessories/`),
+    elements:  {
+      original: getElements(`${dir}/Eyes_Accessories/original`),
+      rare: getElements(`${dir}/Eyes_Accessories/rare`),
+      super_rare: getElements(`${dir}/Eyes_Accessories/super_rare`),
+    },
     position: {x: 0, y: 0},
     size: { width: width, height: height },
   },
   {
     id: 4,
     name: "U-Accessories",
-    location: `${dir}/U-Accessories/`,
-    elements: getElements(`${dir}/U-Accessories/`),
+    elements:  {
+      original: getElements(`${dir}/U-Accessories/original`),
+      rare: getElements(`${dir}/U-Accessories/rare`),
+      super_rare: getElements(`${dir}/U-Accessories/super_rare`),
+    },
     position: {x: 0, y: 0},
     size: { width: width, height: height },
   },
 ];
 
 
-module.exports = { layers, width, height };
+module.exports = { 
+  layers,
+  width, 
+  height, 
+  description, 
+  baseImageUri, 
+  editionSize, 
+  startEditionFrom, 
+  endEditionAt, 
+  rarityWeights,
+};
 
 
 
